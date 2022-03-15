379e83c0017de1558a29265f1d58d0e87106e63f
Elucian Moise <elucian.moise@gmail.com>
2022-03-15 19:46:32 +0000

    create menu

--- main.go
+++ main.go
@@ -1,34 +1,37 @@
 package main
 
 import (
-  test "golang/test"
+  demo "golang/demo"
 )
 
 import (
 	"fmt"
 )
 
-func main() {
-  option := 9
-  // menu
-  fmt.Println("Run demo: ")
+func print_menu() {
   fmt.Println("----------------- ")
   fmt.Println("0 = quit ")        
   fmt.Println("1 = Hello World")
   fmt.Println("2 = For loop")  
-  fmt.Println("----------------- ")
+  fmt.Println("----------------- ")  
+}
+
+func main() {
+  option := 9
+  fmt.Println("Run demo: ")
+  print_menu() 
   for ;option>0; {
     fmt.Print(">>")
     fmt.Scanf("%d",&option)
     if option == 0 {
        break 
     } else if option == 1 {
-      test.Hello()
+      demo.Hello()
     } else if option == 2 {
-      test.For_loop()
+      demo.For_loop()
     } else {
-      fmt.Println("error")
-      break
+      fmt.Println("invalid option")
+      print_menu() 
     }
   }
 }
