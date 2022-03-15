dbf4e32f6cb22f18833f5fd3fab15c30d0fc4ad9
Elucian Moise <elucian.moise@gmail.com>
2022-03-15 19:36:51 +0000

    create REPL menu

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main
+module golang
 
-go 1.16
+go 1.14
--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+  test "golang/test"
+)
+
+import (
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+  option := 9
+  // menu
+  fmt.Println("Run demo: ")
+  fmt.Println("----------------- ")
+  fmt.Println("0 = quit ")        
+  fmt.Println("1 = Hello World")
+  fmt.Println("2 = For loop")  
+  fmt.Println("----------------- ")
+  for ;option>0; {
+    fmt.Print(">>")
+    fmt.Scanf("%d",&option)
+    if option == 0 {
+       break 
+    } else if option == 1 {
+      test.Hello()
+    } else if option == 2 {
+      test.For_loop()
+    } else {
+      fmt.Println("error")
+      break
+    }
+  }
 }
